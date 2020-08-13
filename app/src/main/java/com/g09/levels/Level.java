f2704212c906d051dcf48641ac640414b7172c36
Wojciech Sowa <dzidadzidowski@gmail.com>
2020-08-13 20:09:34 +0200

    Lol nie scommitowałem tej klasy xd

---
+++ app/src/main/java/com/g09/levels/Level.java
@@ -0,0 +1,76 @@
+package com.g09.levels;
+
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.g09.R;
+
+public abstract class Level extends AppCompatActivity implements SensorEventListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(getFlag() ? R.style.AppThemeDark : R.style.AppThemeLight);
+        super.onCreate(savedInstanceState);
+        onCreate();
+    }
+
+    protected abstract void onCreate();
+    protected abstract void start();
+    protected abstract void stop();
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        start();
+    }
+
+    @Override
+    public abstract void onSensorChanged(SensorEvent sensorEvent);
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {}
+
+    protected void noSensorsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("Your device doesn't support the sensors used in level.")
+                .setCancelable(false)
+                .setNegativeButton("Close",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    protected void winAlert(String title, String message) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage(message)
+                .setTitle(title)
+                .setCancelable(false)
+                .setNegativeButton("Ok",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private boolean getFlag() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        return preferences.getBoolean("dark", false);
+    }
+}
