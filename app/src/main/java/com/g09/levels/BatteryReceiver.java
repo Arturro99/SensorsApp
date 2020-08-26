cd04989d577fc23d29677fbee350e1e50ee0f4e2
Daniel <londka99@gmail.com>
2020-08-27 00:30:44 +0200

    Lvl7 works ok, progression doesnt work. Emulator has problems with this lvl, physical devices work ok.

--- app/src/main/java/com/g09/levels/BatteryReceiver.java
+++ app/src/main/java/com/g09/levels/BatteryReceiver.java
@@ -4,31 +4,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
+import android.view.View;
 import android.widget.Button;
 import com.g09.R;
 
+
 public class BatteryReceiver extends BroadcastReceiver {
-    Button btn1, btn2, btn3, btn4;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        btn1 = ((Lvl7)context).findViewById(R.id.button1);
-        btn2 = ((Lvl7)context).findViewById(R.id.button2);
-        btn3 = ((Lvl7)context).findViewById(R.id.button3);
-        btn4 = ((Lvl7)context).findViewById(R.id.button4);
-
-        BatteryManager mBatteryManager = (BatteryManager) context.getSystemService(Context.BATTERY_SERVICE);
-        long batteryCapacity = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER);
-        if (batteryCapacity == Long.MIN_VALUE) batteryCapacity = 3;
-
-        btn1.setText(Long.toString(batteryCapacity-200));
-        btn2.setText(Long.toString(batteryCapacity));
-        btn3.setText(Long.toString(batteryCapacity+200));
-        btn4.setText(Long.toString(batteryCapacity+400));
-
+        Button lvl7btn = ((Lvl7)context).findViewById(R.id.lvl7BTN);
         String action = intent.getAction();
-        if (action != null && action.equals(Intent.ACTION_BATTERY_CHANGED)){
-
+        if (action != null && action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
+                lvl7btn.setVisibility(View.VISIBLE);
+            }
         }
 
     }
--- app/src/main/java/com/g09/levels/Lvl7.java
+++ app/src/main/java/com/g09/levels/Lvl7.java
@@ -5,10 +5,11 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.hardware.SensorEvent;
 import android.media.MediaPlayer;
-import android.os.BatteryManager;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import com.g09.R;
@@ -21,6 +22,8 @@ public class Lvl7 extends Level {
     MediaPlayer mediaPlayer;
     SharedPreferences sharedPreferences;
     TextView lvl7txt;
+    Button lvl7btn;
+    Long timerA;
     private BatteryReceiver batteryReceiver = new BatteryReceiver();
     private IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
 
@@ -29,9 +32,12 @@ public class Lvl7 extends Level {
         setContentView(R.layout.lvl7);
         ImageButton hint = findViewById(R.id.hint7);
         lvl7txt = findViewById(R.id.lvl7txt);
+        lvl7btn = findViewById(R.id.lvl7BTN);
+        lvl7btn.setVisibility(View.INVISIBLE);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
 
-
+        timerA = SystemClock.elapsedRealtime();
+        start();
 
         // Thread for playing the end theme
         Runnable r = new Runnable() {
@@ -44,6 +50,24 @@ public class Lvl7 extends Level {
                 winAlert("Gratulacje", "Udało Ci się ukończyć wszystkie poziomy", null);
             }
         };
+
+        lvl7btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+
+                // Win alert pokazuje poniższą informację i czas w dobry sposób, ale wysypuje się program
+                // winAlert("Gratulacje!", "Udało ci się przejść poziom!\nTwój czas: " + (SystemClock.elapsedRealtime() - timerA)*0.001 + "sekund", Lvl8.class);
+
+                // Progressing past lvl 7
+
+
+                stop();
+
+                // Wątek na end theme nie jest chyba potrzebny
+                // r.run();
+            }
+        });
 
         hint.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,8 +83,6 @@ public class Lvl7 extends Level {
 
     @Override
     protected void start() {
-
-
         registerReceiver(batteryReceiver, intentFilter);
     }
 
